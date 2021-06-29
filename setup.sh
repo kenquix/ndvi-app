2ae7be96d34475851fafe532ffce1405799c29b9
Kenneth Quisado <kaquisado@up.edu.ph>
2021-06-29 17:25:57 +0800

    make it better

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[theme]
+primaryColor="#50cb3c"
+backgroundColor="#efede8"
+secondaryBackgroundColor="#d8d4c3"
+textColor="#424242"
+font = "sans serif"
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
